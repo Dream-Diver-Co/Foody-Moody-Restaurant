4c21d047b97267548cd5aeff01c1833ba2cf9387
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-01-21 17:47:35 +0600

    solar Update

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,23 +5,32 @@ import { images } from "../../constants";
 import "../../constants/script";
 
 const Header = () => (
+  <div className="hero_section">
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="container">
       <div className="sun">
+      
         <img src={images.sun} alt="sun" />
       </div>
-      <div class="mercury"></div>
+      {/* <div class="mercury"></div>
       <div class="venus"></div>
       <div class="earth">
         <div class="moon"></div>
       </div>
       <div class="mars"></div>
       <div class="jupiter"></div>
-      <div class="saturn"></div>
+      <div class="saturn"></div> */}
+      <a href="https://dreamdiver.nl/" class="uranus-link">
       <div class="uranus"></div>
+      </a>
+      <a href="https://facebook.com/" class="uranus-link">
       <div class="neptune"></div>
+      </a>
+      <a href="https://google.com/" class="uranus-link">
       <div class="pluto"></div>
+      </a>
     </div>
+</div>
 </div>
 );
 
