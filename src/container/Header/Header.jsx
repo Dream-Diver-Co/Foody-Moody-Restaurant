81fb552af0d3235ab4533a6fb92a27d7a81ff26b
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-01-29 14:33:43 +0600

    Solar Panel Design Updated

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,32 +4,19 @@ import "./Header.css";
 import { images } from "../../constants";
 import "../../constants/script";
 
-const Header = () => (
-  <div className="hero_section">
-  <div className="app__header app__wrapper section__padding" id="home">
-    <div className="container">
-      <div className="sun">
-      
-        <img src={images.foody_moody_logo} alt="sun" />
+const Header = () => (<div className="app__header app__wrapper section__padding" id="home">
+<div className="app__wrapper_info">
+  <div class="container">
+    <div class="sun">
+    <img src={images.foody_moody_logo} alt="sun" />
       </div>
-      {/* <div class="mercury"></div>
-      <div class="venus"></div>
-      <div class="earth">
-        <div class="moon"></div>
-      </div>
-      <div class="mars"></div>
-      <div class="jupiter"></div>
-      <div class="saturn"></div> */}
-      <a href="https://dreamdiver.nl/" class="uranus-link">
-      <div class="uranus"></div>
-      </a>
-      <a href="https://facebook.com/" class="uranus-link">
-      <div class="neptune"></div>
-      </a>
-      <a href="https://google.com/" class="uranus-link">
-      <div class="pluto"></div>
-      </a>
-    </div>
+    <div class="earth"></div>
+    <div class="jupiter"></div>
+    <div class="saturn"></div>
+    <div class="uranus"></div>
+    <div class="neptune"></div>
+    <div class="pluto"></div>
+  </div>
 </div>
 </div>
 );
